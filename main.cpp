605d66e3d8b5c314a1cb8932adb032681d2d2615
Batiatari Kaze <batiatari.dono@gmail.com>
2015-10-01 20:47:53 +0300

    done

--- main.cpp
+++ main.cpp
@@ -7,13 +7,35 @@
 
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	ifstream stream(argv[1]);
-	string line;
-	while (getline(stream, line)) {
-		cout << line << endl;
+	int n, k, sum;
+	short count;
+	while (stream >> n) {
+		k = n;
+		count = 0;
+		do {
+			count++;
+			k /= 10;
+		} while (k);
+		
+		k = n;
+		
+		sum = 0;
+		while (n) {
+			sum += pow(n%10, count);
+			n /= 10;
+		}
+		
+		if (sum == k) {
+			cout << "True" << endl;
+		}
+		else {
+			cout << "False" << endl;
+		}
 	}
 	return 0;
 }
